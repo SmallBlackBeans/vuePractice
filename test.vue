3564bb54de8c08154fc4fa075ba3298af6460b3c
韩承海 <374802597@qq.com>
2017-12-04 18:32:55 +0800

    no message

---
+++ test.vue
@@ -0,0 +1,48 @@
+
+<template>
+
+<div>
+<span> 文本插值: {{ msg }}</span>
+	<span v-once>这个将不会再改变{{ msg }}</span>
+</div>
+	
+</template>
+
+
+<script>
+	var data = { a: 1}
+	var vm = new Vue({
+		el: '#example',
+		data: {
+			msg: 'nihao hanxiaocu'
+		},
+
+		beforeCreate: function () {
+
+		},
+
+		created: function () {
+			//this 指向的是当前的实例
+			//=> 箭头函数是和父级上下文绑定在一起的，this 不会是如你所预期的 Vue 实例
+			console.log('a is: ' + this.a )
+		},
+
+		beforeMounted: function () {
+
+		},
+
+		mounted: function () {
+
+		},
+
+		updated: function () {
+
+		},
+
+		destroyed: function () {
+
+		},
+
+	})
+
+</script>
---
+++ vueProject/src/Test.vue
@@ -0,0 +1,80 @@
+
+/** 
+    计算属性带有缓存，如果你不希望有缓存，请用方法来替代
+*/
+
+<template>
+    <div id='demo'>
+        {{ fullname }}
+        <h1>Title</h1>
+        <p>Paragraph 1</p>
+        <p>Paragraph 2</p>
+    </div>
+    <div v-if="Math.random() > 0.5">
+        Now you see
+    </div>
+    <div v-else>
+        now you don't
+    </div>
+
+</template>
+
+
+
+
+
+
+<style>
+
+</style>
+
+
+<script src="https://cdn.jsdelivr.net/npm/axios@0.12.0/dist/axios.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/lodash@4.13.1/lodash.min.js"></script>
+<script>
+var watchExampleVM = new Vue({
+    el: '#watch-example',
+    data: {
+        question: '',
+        answer: 'xxxx'
+    },
+    watch: {
+        question: function () {
+            this.answer = 'x,,,,dd',
+            this.getAnswer()
+        }
+    },
+    methods: {
+        a: function() {
+            
+        }
+    }
+})
+
+    var vm = new Vue({
+  el: '#demo'
+  data: {
+    firstname: 'han',
+    lastName: 'chenghai'
+  },
+  computed: {
+    fullname: function () {
+      reture this.firstname + ' ' + this.lastName
+    }
+  }
+
+});
+export default {
+  computed: {
+    fullname: {
+    get: function() {},
+      set: function(newValue) {
+        var names = newValue.spilt(' ')
+        this.firstname = names[0]
+        this.lastName = names[1]
+      }
+    }
+  }
+};
+</script>
+
