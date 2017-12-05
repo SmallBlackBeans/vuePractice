142a0b0c3185868929e059205e7b8a9e1aae68ee
韩承海 <374802597@qq.com>
2017-12-05 11:51:34 +0800

    no message

---
+++ vuepractice/src/components/模板指令.vue
@@ -0,0 +1,23 @@
+<template>
+	<div class="hello">
+		<h1>{{ msg }}</h1>
+		<button v-on:click="onClickMe">click!</button>
+	</div>
+
+
+</template>
+
+
+<script>
+import Header from './header'
+export default {
+	data: function() {
+		return {
+			title: 'this is a title',
+			
+		}
+	}
+} 
+
+
+</script>
